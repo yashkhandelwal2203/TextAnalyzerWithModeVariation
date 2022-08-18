442935cb21bf21a3510ab493253e68b11a6bfe35
Yash Khandelwal <yash.khandelwal456@gmail.com>
2022-08-18 17:40:55 +0530

    Updates

---
+++ precache-manifest.b178822d9c146a39b654f042e519b0cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "03bb65bce26184f35c0b",
+    "url": "/TextAnalyzerWithModeVariation/static/css/main.a73846a6.chunk.css"
+  },
+  {
+    "revision": "03bb65bce26184f35c0b",
+    "url": "/TextAnalyzerWithModeVariation/static/js/main.03bb65bc.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/TextAnalyzerWithModeVariation/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "6bc2ba0d62d024572d5a",
+    "url": "/TextAnalyzerWithModeVariation/static/js/2.6bc2ba0d.chunk.js"
+  },
+  {
+    "revision": "acefe2cf86af9fcdf6f8",
+    "url": "/TextAnalyzerWithModeVariation/static/js/runtime~main.acefe2cf.js"
+  },
+  {
+    "revision": "b18cc5dc040554a96a53d93620859caf",
+    "url": "/TextAnalyzerWithModeVariation/index.html"
+  }
+];
